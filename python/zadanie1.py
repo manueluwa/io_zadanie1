4628c5aacbe4ff06148ad4be0a96b431810b76d4
Manuela Walczak <kolo44876@gmail.com>
2024-03-14 09:34:54 +0100

    pole trapezu

--- python/zadanie1.py
+++ python/zadanie1.py
@@ -7,13 +7,3 @@ bok_c = 3
 h = 5
 pole_trapezu = (bok_b + bok_c) * h / 2
 print("pole trapezu wynosi:" + str(pole_trapezu))
-
-bok_d = 24
-hh = 3
-pole_trojkata = bok_d * hh /2
-print("pole trojkata wynosi:" + str(pole_trojkata))
-
-bok_e = 92
-bok_f = 34
-pole_prostokata = bok_e * bok_f
-print("pole prostokata:" + str(pole_prostokata))
